26abc3b372f27a65f99b9602f618091e7dc086c7
Annelís <juanysacalxot376@gmail.com>
2021-06-25 00:16:44 -0600

    Creacion de ventan y opcion salir

--- src/serpientesyescaleras/SerpientesYEscaleras.java
+++ src/serpientesyescaleras/SerpientesYEscaleras.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package serpientesyescaleras;
+
+import ventana.VentanaPrincipal;
 
 /**
  *
@@ -15,7 +13,9 @@ public class SerpientesYEscaleras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Llamando a mi ventana para que se ejecute
+        
+        VentanaPrincipal.main(args);
     }
     
 }
